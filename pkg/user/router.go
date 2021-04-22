1e5baa89f83825528af220ca3763e562716a3cd4
dreinix <ricardo.aar@outlook.com>
2021-04-22 16:03:12 -0400

    getUser endpoint

--- pkg/user/router.go
+++ pkg/user/router.go
@@ -10,5 +10,7 @@ func ScoreRoutes(db *sql.DB) *chi.Mux {
 	router := chi.NewRouter()
 	router.Get("/", getAllUsers(db))
 	router.Post("/", addUser(db))
+
+	router.Get("/{id}", getUser(db))
 	return router
 }
