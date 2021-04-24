8ed89f1090442293267c45939c5c5d6afcb6d815
dreinix <ricardo.aar@outlook.com>
2021-04-24 12:43:41 -0400

    added cors

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -8,6 +8,7 @@ import (
 	"github.com/dreinix/gonake/pkg/score"
 	"github.com/dreinix/gonake/pkg/user"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func StartServer() *chi.Mux {
@@ -18,7 +19,19 @@ func StartServer() *chi.Mux {
 		fmt.Println(err)
 	}
 	router := chi.NewRouter()
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(cors.Handler)
 
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Gonected to goNake backend"))
+	})
 	router.Mount("/api/scores", score.ScoreRoutes(r))
 	router.Mount("/api/users", user.UserRoutes(r))
 	http.ListenAndServe("127.0.0.1:3001", router)
