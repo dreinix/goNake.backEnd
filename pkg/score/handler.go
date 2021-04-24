9dba8caea567e6af52cee1f0806d986cf2484fd2
dreinix <ricardo.aar@outlook.com>
2021-04-23 23:41:44 -0400

    getAllScores

--- pkg/score/handler.go
+++ pkg/score/handler.go
@@ -3,7 +3,9 @@ package score
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/dreinix/gonake/pkg/auth"
@@ -16,7 +18,47 @@ var (
 
 func getAllScore(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode("Inix")
+		rows, err := db.Query(`SELECT * FROM tbl_score`)
+		if err != nil {
+			w.WriteHeader(500)
+			render.JSON(w, r, "Something went wrong")
+			return
+		}
+		if !rows.Next() {
+			render.JSON(w, r, "no score saved")
+			return
+		}
+		var id int64
+		if err := db.QueryRow(`SELECT * from tbl_score`).
+			Scan(&score.ID, &score.Value, &id, &score.Date); err != nil {
+			render.JSON(w, r, err)
+			return
+		}
+		var user auth.User
+		if err := db.QueryRow(`SELECT usr_id,full_name,usrn FROM tbl_user where usr_id = $1 and stat = $2`, id, "actv").Scan(&user.ID, &user.Name, &user.Username); err != nil {
+			fmt.Println("The user associated with " + strconv.Itoa(score.ID) + " score does not exist anymore.")
+		}
+		var scores []Score
+		//The first value is ignore because "next"
+		score.User = user
+		scores = append(scores, score)
+		for rows.Next() {
+			var s Score
+			err := rows.Scan(&s.ID, &s.Value, &id, &s.Date)
+			if err != nil {
+				w.WriteHeader(500)
+				fmt.Println(err.Error())
+				render.JSON(w, r, "Something went wrong, please try again later")
+				return
+			}
+			if err := db.QueryRow(`SELECT usr_id,full_name,usrn FROM tbl_user where usr_id = $1 and stat = $2`, id, "actv").Scan(&user.ID, &user.Name, &user.Username); err != nil {
+				fmt.Println("The user associated with " + strconv.Itoa(score.ID) + " score does not exist anymore.")
+			}
+			s.User = user
+			scores = append(scores, s)
+		}
+		// if there's only one result there's no next
+		render.JSON(w, r, scores)
 	}
 }
 
--- pkg/score/model.go
+++ pkg/score/model.go
@@ -3,12 +3,12 @@ package score
 import (
 	"time"
 
-	"github.com/dreinix/gonake/pkg/user"
+	"github.com/dreinix/gonake/pkg/auth"
 )
 
 type Score struct {
 	ID    int       `json:"ID"`
 	Value int       `json:"value"`
-	User  user.User `json:"user"`
+	User  auth.User `json:"user"`
 	Date  time.Time `json:"date"`
 }
