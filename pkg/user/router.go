0ce407242f7d01862c8b51240ca6aa62d4a20984
dreinix <ricardo.aar@outlook.com>
2021-05-05 23:40:45 -0400

    Added get me

--- pkg/user/router.go
+++ pkg/user/router.go
@@ -10,11 +10,11 @@ import (
 func UserRoutes(db *sql.DB) *chi.Mux {
 	router := chi.NewRouter()
 	router.Get("/", (getAllUsers(db)))
+	router.Get("/getMe", auth.Authentication(getMe(db)))
+	router.Get("/{id}", auth.Authentication(getUser(db)))
 	router.Post("/", addUser(db))
 	router.Post("/login", logIn(db))
-	router.Get("/{id}", auth.Authentication(getUser(db)))
 	router.Post("/logout", auth.Authentication(LogOut(db)))
-	router.Post("/getMe", auth.Authentication(getMe(db)))
 	return router
 	//auth.Authenticate
 }
